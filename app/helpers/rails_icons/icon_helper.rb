62eac4de24aca0d263dab4ff1488cd2c44bdc661
e <988051+eelcoj@users.noreply.github.com>
2025-11-16 10:06:20 +0000

    Added `encoded_icon` helper (#84)

---
+++ app/helpers/rails_icons/icon_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module RailsIcons
+  module IconHelper
+    def icon(name, library: RailsIcons.configuration.default_library, from: library, variant: nil, **arguments)
+      RailsIcons::Icon.new(
+        name: name,
+        library: from || library,
+        variant: variant,
+        arguments: arguments
+      ).svg
+    end
+
+    def encoded_icon(name, library: RailsIcons.configuration.default_library, from: library, variant: nil, **arguments)
+      svg_content = RailsIcons::Icon.new(
+        name: name,
+        library: from || library,
+        variant: variant,
+        arguments: arguments
+      ).svg
+
+      "data:image/svg+xml;base64,#{Base64.strict_encode64(svg_content)}"
+    end
+  end
+end
